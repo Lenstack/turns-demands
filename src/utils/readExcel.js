918d448b5cc625eb75f62522c2d756b1a3b4f5c4
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-10-17 21:13:28 -0500

    Update

--- src/utils/readExcel.js
+++ src/utils/readExcel.js
@@ -1,9 +1,16 @@
 const xlsx = require('xlsx');
-//TODO: Write the logic to read the excel file
-const readExcel = (file) => {
-    const workbook = xlsx.readFile(file);
-    const sheetName = workbook.SheetNames[0];
+const readExcel = async (filePath) => {
+    try {
+        // Read the Excel file
+        const workbook = await xlsx.readFile(filePath);
+        // Get the first sheet of the Excel workbook
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
 
-    const sheet = workbook.Sheets[sheetName];
-    return xlsx.utils.sheet_to_json(sheet); // return a json object of the excel file
+        // Parse the sheet data to JSON format
+        return xlsx.utils.sheet_to_json(sheet);
+    } catch (error) {
+        console.error('Error reading Excel file:', error.message);
+        return null;
+    }
 }
