2b77e9a8005268f084a98067727ed866556033c1
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-10-17 21:18:38 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-const loadExcel = require('./utils/readExcel')
+const readExcel = require('./utils/readExcel')
 const createUser = require("./core/repositories/createUser");
 const chooseIsAvailable = require('./core/chooseIsAvailable')
 
     // Here you can see the main function that will be executed
     (async () => {
-        /*
-            // If First start the app - Here you will write the logic to load the excel file
-            const file = 'file.xlsx'
-            const excelData = await readExcel(file)
 
-            // Here you will implement the logic to create the users in the database
-            const user = {
-                id: "11000000",
-                contract: "11000000",
-                branchOffices: [],
-                restrictions: [],
-            }
+        // If First start the app - Here you will write the logic to load the excel file
+        const file = 'file.xlsx'
+        const excelData = await readExcel(file)
 
-            // Here you will call the createUser function
-
-            for (const user of excelData) {
-                await createUser(user)
-            }               
-        */
-
-        //
-
-        // Here you call the database and get the users and restrictions
         const users = []
         const restrictions = []
+        for (const excel of excelData) {
+
+            const user = {
+                id: excel.id,
+                contract: excel.contract,
+                branchOffices: excel.branchOffices,
+            }
+
+            const restrictions = {
+                id: excel.id,
+                restriction: excel.restriction
+            }
+
+            // Here you will implement the logic to create the users in the database
+            const userCreated = await createUser(user)
+
+            users.push(userCreated)
+            restrictions.push(restrictions)
+
+        }
+
+        // Here you call the database and get the users and restrictions
+        // const users = await getUsers()
+        // const restrictions = await getRestrictions()
 
         // Here we will get the users and restrictions from the database
         await chooseIsAvailable(users, restrictions)
